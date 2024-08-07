401b2a4c993c6c6234093f33d68cd0cb8b198bbb
CCNiu <79887847+CCNiu@users.noreply.github.com>
2024-08-07 23:32:04 +0800

    audio ok
    the audio for giga is ok

--- USB_storage_test/USB_storage_test.ino
+++ USB_storage_test/USB_storage_test.ino
@@ -1,68 +1,78 @@
-#include <Arduino_USBHostMbed5.h>
 #include <DigitalOut.h>
 #include <FATFileSystem.h>
+#include <Arduino_USBHostMbed5.h>
 
 USBHostMSD msd;
 mbed::FATFileSystem usb("usb");
 
-// mbed::DigitalOut pin5(PC_6, 0);
-mbed::DigitalOut otg(PB_8, 1);
 
-void setup() {
-  Serial.begin(115200);
-  
-  pinMode(PA_15, OUTPUT); //enable the USB-A port
-  digitalWrite(PA_15, HIGH);
-  
-  while (!Serial);
-  
-  msd.connect();
+void setup()
+{
+    Serial.begin(115200);
+    
+    pinMode(PA_15, OUTPUT); //enable the USB-A port
+    digitalWrite(PA_15, HIGH);
+    
+    while (!Serial)
+        ;
 
-  while (!msd.connected()) {
-    //while (!port.connected()) {
-    delay(1000);
-  }
+    Serial.println("Starting USB Dir List example...");
 
-  Serial.println("Mounting USB device...");
-  int err =  usb.mount(&msd);
-  if (err) {
-    Serial.print("Error mounting USB device ");
-    Serial.println(err);
-    while (1);
-  }
-  Serial.print("read done ");
-  mbed::fs_file_t file;
-  struct dirent *ent;
-  int dirIndex = 0;
-  int res = 0;
-  Serial.println("Open /usb/numbers.txt");
-  FILE *f = fopen("/usb/numbers.txt", "w+");
-  for (int i = 0; i < 10; i++) {
-    Serial.print("Writing numbers (");
-    Serial.print(i);
-    Serial.println("/10)");
-    fflush(stdout);
-    err = fprintf(f, "%d\n", i);
-    if (err < 0) {
-      Serial.println("Fail :(");
-      error("error: %s (%d)\n", strerror(errno), -errno);
+    // if you are using a Max Carrier uncomment the following line
+    // start_hub();
+
+    while (!msd.connect()) {
+        //while (!port.connected()) {
+        delay(1000);
     }
-  }
 
-  Serial.println("File closing");
-  fflush(stdout);
-  err = fclose(f);
-  if (err < 0) {
-    Serial.print("fclose error:");
-    Serial.print(strerror(errno));
-    Serial.print(" (");
-    Serial.print(-errno);
-    Serial.print(")");
-  } else {
-    Serial.println("File closed");
-  }
+    Serial.print("Mounting USB device... ");
+    int err = usb.mount(&msd);
+    if (err) {
+        Serial.print("Error mounting USB device ");
+        Serial.println(err);
+        while (1);
+    }
+    Serial.println("done.");
+
+    char buf[256];
+
+    // Display the root directory
+    Serial.print("Opening the root directory... ");
+    DIR* d = opendir("/usb/");
+    Serial.println(!d ? "Fail :(" : "Done");
+    if (!d) {
+        snprintf(buf, sizeof(buf), "error: %s (%d)\r\n", strerror(errno), -errno);
+        Serial.print(buf);
+    }
+    Serial.println("done.");
+
+    Serial.println("Root directory:");
+    unsigned int count { 0 };
+    while (true) {
+        struct dirent* e = readdir(d);
+        if (!e) {
+            break;
+        }
+        count++;
+        snprintf(buf, sizeof(buf), "    %s\r\n", e->d_name);
+        Serial.print(buf);
+    }
+    Serial.print(count);
+    Serial.println(" files found!");
+
+    snprintf(buf, sizeof(buf), "Closing the root directory... ");
+    Serial.print(buf);
+    fflush(stdout);
+    err = closedir(d);
+    snprintf(buf, sizeof(buf), "%s\r\n", (err < 0 ? "Fail :(" : "OK"));
+    Serial.print(buf);
+    if (err < 0) {
+        snprintf(buf, sizeof(buf), "error: %s (%d)\r\n", strerror(errno), -errno);
+        Serial.print(buf);
+    }
 }
 
-void loop() {
-
+void loop()
+{
 }
