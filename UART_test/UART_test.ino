4f7f962d4f0bceaf03beb0f7a5aa719279e79229
CCNiu <79887847+CCNiu@users.noreply.github.com>
2024-08-31 18:55:27 +0800

    Update UART_test.ino

--- UART_test/UART_test.ino
+++ UART_test/UART_test.ino
@@ -1,39 +1,3 @@
-// void setup() {
-//   // 初始化串口
-//   Serial.begin(9600); // 用於監控
-//   Serial1.begin(9600); // 用於UART通訊
-
-//   // 等待串口初始化穩定
-//   delay(2000);
-
-//   // 確認串口初始化
-//   Serial.println("UART Communication Initialized");
-// }
-
-// void loop() {
-//   // 檢查是否有來自Serial1的數據
-//   if (Serial1.available()) {
-//     // 讀取數據，直到換行符結束
-//     String receivedData = Serial1.readStringUntil('\n');
-    
-//     // 顯示接收到的數據
-//     Serial.print("Received: ");
-//     Serial.println(receivedData);
-
-//     // 回應數據
-//     Serial1.print("Data received: ");
-//     Serial1.println(receivedData);
-//   }
-
-//   // 如果從串口監控器輸入數據，將其發送到Serial1
-//   if (Serial.available()) {
-//     // 讀取數據，直到換行符結束
-//     String dataToSend = Serial.readStringUntil('\n');
-//     Serial1.print(dataToSend);
-//     Serial.print("KOKOK");
-//     Serial1.println(); // 添加換行符
-//   }
-// }
 #include <HardwareSerial.h>
 String receivedData = "";
 void setup() {
@@ -49,16 +13,20 @@ void loop() {
   if (Serial2.available()) {
     // 讀取數據，直到換行符結束
     receivedData = Serial2.readStringUntil('\n');
-    // Serial.println("2");
     // 顯示接收到的數據
+    delay(10);
     Serial.print("Received: ");
     Serial.println(receivedData);
   }
-  delay(1000);
+  delay(10);
   if (Serial.available()) {
     // 讀取數據，直到換行符結束
     String dataToSend = Serial.readStringUntil('\n');
-    Serial2.print(dataToSend);
-    Serial.print(dataToSend);
+    delay(10);
+    Serial2.print(dataToSend+'\n');
+    // size_t temp = Serial2.print(dataToSend);
+    Serial.println(dataToSend);
+    // Serial.println(temp);
+    Serial.println(dataToSend.length());
   }
 }
